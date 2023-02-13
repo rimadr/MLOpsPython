ad6dd913988df59abdac993327b06e3cd804dace
Rima Dridi <rimadridi@MacBook-Air-de-Rima.local>
2023-02-13 17:03:30 +0100

    Test pull request3

---
+++ train/extraction/tests/extraction_test.py
@@ -0,0 +1,10 @@
+import unittest
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        value = 'foo'.upper()
+        expected = 'FOO'
+        self.assertEqual(value, expected)
+
+if __name__ == '__main__':
+    unittest.main()
